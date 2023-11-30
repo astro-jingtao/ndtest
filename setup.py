e3f269b2ddd85056daebfe29feb5803ca1e2f799
MySlientWind <634790985@qq.com>
2023-11-30 19:29:17 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
+
+f2py_compile = Extension(
+    name='ndtest.maxdist',  # Name of the extension module
+    sources=['ndtest/maxdist.f90'],  # Fortran source file(s)
+    extra_compile_args=['-O3'],  # Additional compilation options if needed
+    f2py_options=['--quiet'],  # f2py-specific options
+)
 
 setup(
     name="ndtesst",
     version="0.1",
     author="Tao Jing",
     author_email="jingt20@mails.tsinghua.edu.cn",
-    description="The collection of nonparametric test of the equality of high-dimensional probability distributions",
-    packages=find_packages()
-)
+    description=
+    "The collection of nonparametric test of the equality of high-dimensional probability distributions",
+    ext_modules=[f2py_compile],
+    packages=find_packages())
