580738b0619c272aef882ce496b57997b80a7a10
astro-jingtao <634790985@qq.com>
2024-10-29 11:29:23 +0800

    use python -m numpy.f2py for windows

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,14 @@
 # Changelog
 
-
 ## [unreleased]
 
 ### Added
 
 - Raise an error if the compiling of FORTRAN fails.
+
+### Changed
+
+- Use `python -m numpy.f2py` to compile FORTRAN code, thus it can be used on Windows.
 
 ## 0.1.0 - 2024-10-29
 
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class f2py_Build(build_ext):
         for to_compile in ext.sourcedirs:
             module_name = os.path.split(to_compile)[1].split('.')[0]
             result = os.system(
-                f'cd {build_lib}/{ext.name};f2py -c {to_compile} -m {module_name} --f90flags=-O3'
+                f'cd {build_lib}/{ext.name};python -m numpy.f2py -c {to_compile} -m {module_name} --f90flags=-O3'
             )
             if result != 0:
                 raise RuntimeError(f'Failed to compile {to_compile}')
